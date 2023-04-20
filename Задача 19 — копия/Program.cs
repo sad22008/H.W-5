e312f493c0556e64b8eb4439a532c197ff09529e
Pavel <sad22008@yandex.ru>
2023-04-20 16:51:38 +0300

    Добавили файл

---
+++ Задача 19 — копия/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Напишите программу,которая упорядочивает 
+// по убыванию элементы каждой строки двумерного массива.
+
+Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m, n];
+
+void mas(int m, int n)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 9);
+        }
+    }
+}
+
+void printm(int[,] array)
+{
+    int i, j;
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+mas(m, n);
+Console.WriteLine("\nИсходный массив: ");
+printm(randomArray);
+
+// Процедура сортировки элементов в строке двумерного массива по убыванию
+void uporyadok(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+uporyadok(randomArray);
+Console.WriteLine("\nОтсортированный массив: ");
+printm(randomArray);
---
+++ Задача 21 — копия/Program.cs
@@ -0,0 +1,104 @@
+﻿//  Задайте две матрицы.
+//  Напишите программу, которая будет находить произведение двух матриц.
+
+using System;
+
+// класс с методами расширения
+static class MatrixExt
+{
+    // метод расширения для получения количества строк матрицы
+    public static int RowsCount(this int[,] matrix)
+    {
+        return matrix.GetUpperBound(0) + 1;
+    }
+
+    // метод расширения для получения количества столбцов матрицы
+    public static int ColumnsCount(this int[,] matrix)
+    {
+        return matrix.GetUpperBound(1) + 1;
+    }
+}
+
+class Program
+{
+    // метод для получения матрицы из консоли
+    static int[,] GetMatrixFromConsole(string name)
+    {
+        Console.Write("Количество строк матрицы {0}:    ", name);
+        var n = int.Parse(Console.ReadLine());
+        Console.Write("Количество столбцов матрицы {0}: ", name);
+        var m = int.Parse(Console.ReadLine());
+
+        var matrix = new int[n, m];
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                Console.Write("{0}[{1},{2}] = ", name, i, j);
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        return matrix;
+    }
+
+    // метод для печати матрицы в консоль
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (var i = 0; i < matrix.RowsCount(); i++)
+        {
+            for (var j = 0; j < matrix.ColumnsCount(); j++)
+            {
+                Console.Write(matrix[i, j].ToString().PadLeft(4));
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    // метод для умножения матриц
+    static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {
+        if (matrixA.ColumnsCount() != matrixB.RowsCount())
+        {
+            throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+        }
+
+        var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+        for (var i = 0; i < matrixA.RowsCount(); i++)
+        {
+            for (var j = 0; j < matrixB.ColumnsCount(); j++)
+            {
+                matrixC[i, j] = 0;
+
+                for (var k = 0; k < matrixA.ColumnsCount(); k++)
+                {
+                    matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+
+        return matrixC;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Программа для умножения матриц");
+
+        var a = GetMatrixFromConsole("A");
+        var b = GetMatrixFromConsole("B");
+
+        Console.WriteLine("Матрица A:");
+        PrintMatrix(a);
+
+        Console.WriteLine("Матрица B:");
+        PrintMatrix(b);
+
+        var result = MatrixMultiplication(a, b);
+        Console.WriteLine("Произведение матриц:");
+        PrintMatrix(result);
+
+        Console.ReadLine();
+    }
+}
